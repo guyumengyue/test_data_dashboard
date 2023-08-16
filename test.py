c9fc804f9531840ef11f7645171efc14a180f45c
guyumengyue <53812603+guyumengyue@users.noreply.github.com>
2023-08-16 14:34:19 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pandas_profiling
-from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(layout="wide")
 
